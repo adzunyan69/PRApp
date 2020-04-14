03a6c7b4e0f60e20677219b4e1c751e363264e73
adzunyan69 <kruglakovartem@gmail.com>
2020-04-15 01:17:26 +0300

    Создан временный проект нейронки
    Временный проект/папка для попыток интегрирования нейронки и классификации изображений

---
+++ NeurlaNetworkModuleTesting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NeurlaNetworkModuleTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
